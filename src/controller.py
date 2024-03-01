0d9f931d0600b95eedcea0d86cd9c1a68303934e
Gustavo <gustqvomachadoresende@gmail.com>
2024-02-29 23:30:51 -0300

    feat: conectando com a swapi

--- src/controller.py
+++ src/controller.py
@@ -1,9 +1,19 @@
 # controller.py
 from fastapi import APIRouter
+import requests
 
 router = APIRouter()
 
-@router.get("/")
-def controller_test():
-    test = 'oi'
-    return test
+class Controller:
+    # def __init__(self) -> None:
+    #     self.name = "Gustavo"
+
+    @router.get("/test")
+    def controller_test(name: str) -> None:
+        try:
+            url: str =  f'https://swapi.dev/api/{name}/'
+            url_connection = requests.get(url)
+            test = url_connection 
+            return test
+        except Exception as e:
+            raise e
