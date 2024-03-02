e827db97544bc36ee2df5413413e2df26c1568c4
Gustavo <gustqvomachadoresende@gmail.com>
2024-03-02 18:02:25 -0300

    feat: adicionando mangnum para poder utilizar o lambda

--- api/main.py
+++ api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-# from mangum import Mangum # TESTE
+from mangum import Mangum
 from api.src.controller import router
 
 app = FastAPI()
-# handler = Mangum(app)
-
 app.include_router(router)
+
+handler = Mangum(app)
