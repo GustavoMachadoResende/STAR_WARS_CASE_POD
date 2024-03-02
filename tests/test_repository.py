b8d088015d549c18a22c09948d50215db3ea691d
Gustavo <gustqvomachadoresende@gmail.com>
2024-03-02 15:59:56 -0300

    test: implemetando camada inicial de testes

---
+++ tests/test_repository.py
@@ -0,0 +1,13 @@
+import unittest
+from src.repository import soma
+
+
+class TestRepository(unittest.TestCase):
+
+    def test_soma(self):
+        result = soma(5, 3)
+        expected = 8
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
