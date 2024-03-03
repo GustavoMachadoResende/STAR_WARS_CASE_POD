a1cb0b02eea14cd2643a0feed0a6501e40bdbbb5
Gustavo <gustqvomachadoresende@gmail.com>
2024-03-03 20:43:03 -0300

    test: implementando teste unitário para função test_get_all_star_wars_data

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -180,5 +180,111 @@ class TestRepositoryStarWars(unittest.TestCase):
         self.assertIsInstance(result, dict)
         self.assertEqual(result, expected_result)
 
+    def test_get_all_star_wars_data(self):
+        result = self.repository.get_all_star_wars_data()
+        expected_result = {
+            "Character - Yoda": {
+                "name": "Yoda",
+                "hair_color": "white",
+                "skin_color": "green",
+                "eye_color": "brown",
+                "gender": "male",
+                "birth_year": "896BBY",
+                "home_planet": "unknown",
+                "films": [
+                "The Empire Strikes Back",
+                "Return of the Jedi",
+                "The Phantom Menace",
+                "Attack of the Clones",
+                "Revenge of the Sith"
+                ],
+                "starships": []
+            },
+            "Planet - Tatooine": {
+                "name": "Tatooine",
+                "population": "200000",
+                "climate": "arid",
+                "gravity": "1 standard",
+                "terrain": "desert",
+                "residents": [
+                "Luke Skywalker",
+                "C-3PO",
+                "Darth Vader",
+                "Owen Lars",
+                "Beru Whitesun lars",
+                "R5-D4",
+                "Biggs Darklighter",
+                "Anakin Skywalker",
+                "Shmi Skywalker",
+                "Cliegg Lars"
+                ],
+                "films": [
+                "A New Hope",
+                "Return of the Jedi",
+                "The Phantom Menace",
+                "Attack of the Clones",
+                "Revenge of the Sith"
+                ]
+            },
+            "Starship - Executor": {
+                "name": "Executor",
+                "model": "Executor-class star dreadnought",
+                "starship_class": "Star dreadnought",
+                "passengers": "38000",
+                "cost_in_credits": "1143350000",
+                "length": "19000",
+                "consumables": "6 years",
+                "pilots": [],
+                "films": [
+                "The Empire Strikes Back",
+                "Return of the Jedi"
+                ]
+            },
+            "Film - A New Hope": {
+                "title": "A New Hope",
+                "opening_crawl": "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
+                "director": "George Lucas",
+                "producer": "Gary Kurtz, Rick McCallum",
+                "release_date": "1977-05-25",
+                "characters": [
+                "Luke Skywalker",
+                "C-3PO",
+                "R2-D2",
+                "Darth Vader",
+                "Leia Organa",
+                "Owen Lars",
+                "Beru Whitesun lars",
+                "R5-D4",
+                "Biggs Darklighter",
+                "Obi-Wan Kenobi",
+                "Wilhuff Tarkin",
+                "Chewbacca",
+                "Han Solo",
+                "Greedo",
+                "Jabba Desilijic Tiure",
+                "Wedge Antilles",
+                "Jek Tono Porkins",
+                "Raymus Antilles"
+                ],
+                "planets": [
+                "Tatooine",
+                "Alderaan",
+                "Yavin IV"
+                ],
+                "starships": [
+                "CR90 corvette",
+                "Star Destroyer",
+                "Sentinel-class landing craft",
+                "Death Star",
+                "Millennium Falcon",
+                "Y-wing",
+                "X-wing",
+                "TIE Advanced x1"
+                ]
+            }
+        }
+
+        self.assertIsInstance(result, dict)
+        self.assertEqual(result, expected_result)
 if __name__ == '__main__':
     unittest.main()
