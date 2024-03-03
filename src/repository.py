0fc5e8397ab28b207b64548bf5d2bb38918d2a16
Gustavo <gustqvomachadoresende@gmail.com>
2024-03-03 18:36:28 -0300

    fix: utilizando princípios de classe e design de código

--- src/repository.py
+++ src/repository.py
@@ -1,15 +1,14 @@
-#VOLTAR DO FOR PARA PASSAR PELAS URLS DO ARRAY
 import json
-from fastapi import HTTPException
 import requests
+from fastapi import HTTPException
 class RepositoryStarWars:
-    def __init__(self, character: str, planet, starship: str, film: str) -> None:
+    def __init__(self, character: str, planet: str, starship: str, film: str) -> None:
         self.character = character
         self.planet = planet
         self.starship = starship
         self.film = film
 
-    def get_data_from_swapi(self, context: str = None, value: str = None, complete_url: str = None):
+    def _get_data_from_swapi(self, context: str = None, value: str = None, complete_url: str = None): # TODO test
         try:
             if complete_url:
                 response = requests.get(complete_url)
@@ -27,9 +26,27 @@ class RepositoryStarWars:
         except Exception as e:
              raise e
 
-    def get_character_data(self, character: str):
+    def _get_data_of_complete_url_from_swapi(self, url: list|str, context: str): # TODO test
         try:
-            character_data = self.get_data_from_swapi(context='people', value=character)
+            if isinstance(url, list):
+                data_list = ([])
+                for url_item in url:
+                    data = self._get_data_from_swapi(complete_url=url_item)
+                    if context == 'films':
+                        data_list.append(data['title'])
+                    else:
+                        data_list.append(data['name'])
+                return data_list
+
+            if isinstance(url, str):
+                 data = self._get_data_from_swapi(complete_url=url)
+                 return data['name']
+        except Exception as e:
+            raise e
+
+    def _get_character_data(self): # TODO test
+        try:
+            character_data = self._get_data_from_swapi(context='people', value=self.character)
             dict_name =  f"Character - {character_data['name']}"
             character_data_dict = {
                 dict_name: {
@@ -39,18 +56,18 @@ class RepositoryStarWars:
                     'eye_color': character_data['eye_color'],
                     'gender': character_data['gender'],
                     'birth_year': character_data['birth_year'],
-                    'home_planet': self.get_data_of_complete_url(url=character_data['homeworld'], context='planets'),
-                    'films': self.get_data_of_complete_url(url=character_data['films'], context='films'),
-                    'starships': self.get_data_of_complete_url(url=character_data['starships'], context='starships')
+                    'home_planet': self._get_data_of_complete_url_from_swapi(url=character_data['homeworld'], context='planets'),
+                    'films': self._get_data_of_complete_url_from_swapi(url=character_data['films'], context='films'),
+                    'starships': self._get_data_of_complete_url_from_swapi(url=character_data['starships'], context='starships')
                 }
             }
             return character_data_dict
         except Exception as e:
                 raise e
 
-    def get_planet_data(self, planet: str):
+    def _get_planet_data(self): # TODO test
         try:
-            planet_data = self.get_data_from_swapi(context='planets', value=planet)
+            planet_data = self._get_data_from_swapi(context='planets', value=self.planet)
             dict_name =  f"Planet - {planet_data['name']}"
             planet_data_dict = {
                 dict_name: {
@@ -59,18 +76,17 @@ class RepositoryStarWars:
                     'climate': planet_data['climate'],
                     'gravity': planet_data['gravity'],
                     'terrain': planet_data['terrain'],
-                    'residents': self.get_data_of_complete_url(url=planet_data['residents'], context='people'),
-                    'films': self.get_data_of_complete_url(url=planet_data['films'], context='films')
-                    # TODO ver de colocar as pessoas
+                    'residents': self._get_data_of_complete_url_from_swapi(url=planet_data['residents'], context='people'),
+                    'films': self._get_data_of_complete_url_from_swapi(url=planet_data['films'], context='films')
                 }
             }
             return planet_data_dict
         except Exception as e:
                 raise e
 
-    def get_starship_data(self, starship: str):
+    def _get_starship_data(self): # TODO test
         try:
-            starship_data = self.get_data_from_swapi(context='starships', value=starship)
+            starship_data = self._get_data_from_swapi(context='starships', value=self.starship)
             dict_name =  f"Starship - {starship_data['name']}"
             starship_data_dict = {
                 dict_name: {
@@ -81,17 +97,17 @@ class RepositoryStarWars:
                     'cost_in_credits': starship_data['cost_in_credits'],
                     'length': starship_data['length'],
                     'consumables': starship_data['consumables'],
-                    'pilots': self.get_data_of_complete_url(url=starship_data['pilots'], context='people'),
-                    'films': self.get_data_of_complete_url(url=starship_data['films'], context='films')
+                    'pilots': self._get_data_of_complete_url_from_swapi(url=starship_data['pilots'], context='people'),
+                    'films': self._get_data_of_complete_url_from_swapi(url=starship_data['films'], context='films')
                 }
             }
             return starship_data_dict
         except Exception as e:
                 raise e
 
-    def get_film_data(self, film: str):
+    def _get_film_data(self): # TODO test
         try:
-            film_data = self.get_data_from_swapi(context='films', value=film)
+            film_data = self._get_data_from_swapi(context='films', value=self.film)
             dict_name =  f"Film - {film_data['title']}"
             film_data_dict = {
                 dict_name: {
@@ -100,51 +116,33 @@ class RepositoryStarWars:
                     'director': film_data['director'],
                     'producer': film_data['producer'],
                     'release_date': film_data['release_date'],
-                    'characters': self.get_data_of_complete_url(url=film_data['characters'], context='people'),
-                    'planets': self.get_data_of_complete_url(url=film_data['planets'], context='planets'),
-                    'starships': self.get_data_of_complete_url(url=film_data['starships'], context='starships')
+                    'characters': self._get_data_of_complete_url_from_swapi(url=film_data['characters'], context='people'),
+                    'planets': self._get_data_of_complete_url_from_swapi(url=film_data['planets'], context='planets'),
+                    'starships': self._get_data_of_complete_url_from_swapi(url=film_data['starships'], context='starships')
                 }
             }
             return film_data_dict
         except Exception as e:
                 raise e
 
-    def get_data_of_complete_url(self, url: list|str, context: str):
-        try:
-            if isinstance(url, list):
-                data_list = ([])
-                for url_item in url:
-                    data = self.get_data_from_swapi(complete_url=url_item)
-                    if context == 'films':
-                        data_list.append(data['title'])
-                    else:
-                        data_list.append(data['name'])
-                return data_list
-
-            if isinstance(url, str):
-                 data = self.get_data_from_swapi(complete_url=url)
-                 return data['name']
-        except Exception as e:
-            raise e
-
-    def all_data_about_star_wars(self):
+    def all_data_about_star_wars(self): # TODO test TALVEZ
         try:
             data_list = ([])
 
             if self.character and isinstance(self.character, str):
-                character_data = self.get_character_data(character=self.character)
+                character_data = self._get_character_data()
                 data_list.append(character_data)
 
             if self.planet and isinstance(self.planet, str):
-                planet_data = self.get_planet_data(planet=self.planet)
+                planet_data = self._get_planet_data()
                 data_list.append(planet_data)
 
             if self.starship and isinstance(self.starship, str):
-                starship_data = self.get_starship_data(starship=self.starship)
+                starship_data = self._get_starship_data()
                 data_list.append(starship_data)
 
             if self.film and isinstance(self.film, str):
-                film_data = self.get_film_data(film=self.film)
+                film_data = self._get_film_data()
                 data_list.append(film_data)
 
             if not self.character and not self.planet and not self.starship and not self.film:
