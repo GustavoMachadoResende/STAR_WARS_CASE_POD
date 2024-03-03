e53360cfb144c00c7044df529fc59988560f5281
Gustavo <gustqvomachadoresende@gmail.com>
2024-03-03 19:38:04 -0300

    test: implementando teste unitário da função _get_film_data

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -130,5 +130,55 @@ class TestRepositoryStarWars(unittest.TestCase):
         self.assertIsInstance(result, dict)
         self.assertEqual(result, expected_result)
 
+    def test_get_film_data(self):
+        result = self.repository._get_film_data()
+        expected_result = {
+            "Film - A New Hope": {
+                "title": "A New Hope",
+                "opening_crawl": "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
+                "director": "George Lucas",
+                "producer": "Gary Kurtz, Rick McCallum",
+                "release_date": "1977-05-25",
+                "characters": [
+                "Luke Skywalker",
+                "C-3PO",
+                "R2-D2",
+                "Darth Vader",
+                "Leia Organa",
+                "Owen Lars",
+                "Beru Whitesun lars",
+                "R5-D4",
+                "Biggs Darklighter",
+                "Obi-Wan Kenobi",
+                "Wilhuff Tarkin",
+                "Chewbacca",
+                "Han Solo",
+                "Greedo",
+                "Jabba Desilijic Tiure",
+                "Wedge Antilles",
+                "Jek Tono Porkins",
+                "Raymus Antilles"
+                ],
+                "planets": [
+                "Tatooine",
+                "Alderaan",
+                "Yavin IV"
+                ],
+                "starships": [
+                "CR90 corvette",
+                "Star Destroyer",
+                "Sentinel-class landing craft",
+                "Death Star",
+                "Millennium Falcon",
+                "Y-wing",
+                "X-wing",
+                "TIE Advanced x1"
+                ]
+            }
+        }
+
+        self.assertIsInstance(result, dict)
+        self.assertEqual(result, expected_result)
+
 if __name__ == '__main__':
     unittest.main()
