f4b37a8077f8ef2bc8a591f28f91a00be8301253
Gustavo <gustqvomachadoresende@gmail.com>
2024-03-03 22:24:21 -0300

    feat: trazendo verificação na controller

--- src/controller.py
+++ src/controller.py
@@ -1,12 +1,17 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src.repository import RepositoryStarWars
 
 router = APIRouter()
 
-@router.get("/starwars-data")
+@router.get("/starwars-data", tags=["Star Wars"])
 def controller_get_all_about_star_wars(character: Optional[str] = None, planet: Optional[str] = None, starship: Optional[str] = None, film: Optional[str] = None):
     try:
+        if not any([character, planet, starship, film]):
+            raise HTTPException(status_code=400, detail={
+                'message': 'Please search for at least one of the fields to get your Star Wars information.',
+            })
+
         repository: RepositoryStarWars = RepositoryStarWars(character=character, planet=planet, starship=starship, film=film)
         result: dict = repository.get_all_star_wars_data()
 
